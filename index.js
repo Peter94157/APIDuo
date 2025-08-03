052b4454cd8d9b75874469b20100f9f1229e9ac0
Pedro <pedroleonardo2001@gmail.com>
2025-08-02 22:49:40 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const express = require("express")
 const app = express()
 const cors = require('cors')
-const port = 5500
+const PORT = process.env.PORT || 5500
 app.use(express.json())
 app.use(cors())
 
